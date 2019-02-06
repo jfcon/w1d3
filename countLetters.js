ebe9c5aa22cc3cbb95de4c5d4722730f2579859a
John <connollyjohnf@gmail.com>
2019-02-06 19:24:45 +0000

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,10 @@
+function countLetters(phrase){
+var oneString = phrase.split(" ").join("");
+console.log(oneString);
+return oneString;
+}
+
+
+
+
+console.log(countLetters("lighthouse in the house"));
