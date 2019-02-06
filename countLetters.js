402c387918c8aa06cd0487b3e1005ccb4d903db8
John <connollyjohnf@gmail.com>
2019-02-06 21:06:28 +0000

    finished countletters program

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,31 @@
-function countLetters(phrase){
-var oneString = phrase.split(" ").join("");
-console.log(oneString);
-return oneString;
+function countLetters(phrase) {
+    var oneString = phrase.split(" ").join("");
+    // console.log('oneString ', oneString);
+    var library = {};
+
+    for(let i=0; i < oneString.length; i++) {
+        // console.log("char ", oneString[i]);
+        if(library[oneString[i]]) {
+            library[oneString[i]]++;
+        } else {
+            library[oneString[i]] = 1;
+        }
+    }
+    return library;
+}
+var result = countLetters("lighthouse in the house");
+
+function format(result){
+    var str = "{ "
+    for(let key in result) {
+        // key
+        // result[key]
+        //str += "\n" + key + ": " + result[key] + ", \n" 
+        str += `\n${key}: ${result[key]}, \n` 
+    }
+    str += "}"
+    return str;
 }
 
-
-
-
-console.log(countLetters("lighthouse in the house"));
+console.log(format(result))
+// console.log(result);
